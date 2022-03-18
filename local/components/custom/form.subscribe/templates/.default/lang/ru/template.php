8196735f2f9eb0e4bff5e7d02d9ccdc63e5f9f5b
FTP <noreply@outcode.ru>
2022-03-18 09:39:03 +0300

    Создание раздела - блог - 10

--- local/components/custom/form.subscribe/templates/.default/lang/ru/template.php
+++ local/components/custom/form.subscribe/templates/.default/lang/ru/template.php
@@ -1,5 +1,5 @@
 <?
 $MESS ['FORM_SUBSCRIBE_TITLE'] = 'Подпишитесь на <span>Fitness</span> рассылку';
-$MESS ['FORM_SUBSCRIBE_AGREEMENT'] = 'Согласен с <a href="/upload/form/Политика_конфиденциальности.pdf">политикой</a> безопасности';
+$MESS ['FORM_SUBSCRIBE_AGREEMENT'] = 'Согласен с <a href="/upload/form/Политика_конфиденциальности.pdf" target="_blank">политикой</a> безопасности';
 $MESS ['FORM_SUBSCRIBE_SEND'] = 'Подписаться';
 $MESS ['FORM_SUBSCRIBE_PLACEHOLDER'] = 'Введите ваш email';
