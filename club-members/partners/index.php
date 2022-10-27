f8039438f7d61d63a37c9da92ce00c2b08eb22dd
Ilvir <i.harisov@spiritfit.ru>
2022-10-27 22:47:43 +0300

    Страница партнеров (исправления)

--- club-members/partners/index.php
+++ club-members/partners/index.php
@@ -247,6 +247,7 @@ $APPLICATION->IncludeFile("/local/include/service/header.php", $includeParams);
                 </div>
             </div>
             <div class="partners-abonements-block">
+                <?if (!empty($objects[$ELEMENT_ID]["PROPERTIES"]["PARTNERS_ABONEMENT_DESC"]["VALUE"]["TEXT"])):?>
                 <div class="partners-abonement__column" style="padding: 0 50px;">
                     <div class="partners-abonements__desc">
                         <?=htmlspecialcharsback($objects[$ELEMENT_ID]["PROPERTIES"]["PARTNERS_ABONEMENT_DESC"]["VALUE"]["TEXT"])?>
@@ -311,16 +312,82 @@ $APPLICATION->IncludeFile("/local/include/service/header.php", $includeParams);
                             "SORT_ORDER2" => "ASC",
                             "STRICT_SECTION_CHECK" => "N",
                             "ITEMS_ID"=>$objects[$ELEMENT_ID]["PROPERTIES"]["PARTNERS_ABONEMENT_ELEMENTS"]["VALUE"],
-                            "SLIDES_TO_SHOW_AND_SCROLL"=>1
+                            "SLIDES_TO_SHOW_AND_SCROLL"=>1,
+                            "CONTENT_CENTER"=>false
                         )
                     );?>
                     </div>
                 </div>
+                <?else:?>
+                    <div class="partners-abonements">
+                        <?$APPLICATION->IncludeComponent(
+                            "bitrix:news.list",
+                            "abonements",
+                            Array(
+                                "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                                "ADD_SECTIONS_CHAIN" => "Y",
+                                "AJAX_MODE" => "N",
+                                "AJAX_OPTION_ADDITIONAL" => "",
+                                "AJAX_OPTION_HISTORY" => "N",
+                                "AJAX_OPTION_JUMP" => "N",
+                                "AJAX_OPTION_STYLE" => "Y",
+                                "CACHE_FILTER" => "N",
+                                "CACHE_GROUPS" => "Y",
+                                "CACHE_TIME" => "36000000",
+                                "CACHE_TYPE" => "A",
+                                "CHECK_DATES" => "Y",
+                                "DETAIL_URL" => "",
+                                "DISPLAY_BOTTOM_PAGER" => "Y",
+                                "DISPLAY_DATE" => "Y",
+                                "DISPLAY_NAME" => "Y",
+                                "DISPLAY_PICTURE" => "Y",
+                                "DISPLAY_PREVIEW_TEXT" => "Y",
+                                "DISPLAY_TOP_PAGER" => "N",
+                                "FIELD_CODE" => array("",""),
+                                "FILE_404" => "",
+                                "FILTER_NAME" => "arrFilterAbonement",
+                                "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                                "IBLOCK_ID" => "9",
+                                "IBLOCK_TYPE" => "content",
+                                "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                                "INCLUDE_SUBSECTIONS" => "Y",
+                                "MESSAGE_404" => "",
+                                "NEWS_COUNT" => "20",
+                                "PAGER_BASE_LINK_ENABLE" => "N",
+                                "PAGER_DESC_NUMBERING" => "N",
+                                "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                                "PAGER_SHOW_ALL" => "N",
+                                "PAGER_SHOW_ALWAYS" => "N",
+                                "PAGER_TEMPLATE" => ".default",
+                                "PAGER_TITLE" => "Новости",
+                                "PARENT_SECTION" => "",
+                                "PARENT_SECTION_CODE" => "",
+                                "PREVIEW_TRUNCATE_LEN" => "",
+                                "PROPERTY_CODE" => array("SIZE","PRICE",""),
+                                "SET_BROWSER_TITLE" => "N",
+                                "SET_LAST_MODIFIED" => "N",
+                                "SET_META_DESCRIPTION" => "N",
+                                "SET_META_KEYWORDS" => "N",
+                                "SET_STATUS_404" => "N",
+                                "SET_TITLE" => "N",
+                                "SHOW_404" => "N",
+                                "SORT_BY1" => "ACTIVE_FROM",
+                                "SORT_BY2" => "SORT",
+                                "SORT_ORDER1" => "ASC",
+                                "SORT_ORDER2" => "ASC",
+                                "STRICT_SECTION_CHECK" => "N",
+                                "ITEMS_ID"=>$objects[$ELEMENT_ID]["PROPERTIES"]["PARTNERS_ABONEMENT_ELEMENTS"]["VALUE"],
+                                "SLIDES_TO_SHOW_AND_SCROLL"=>4,
+                                "CONTENT_CENTER"=>false
+                            )
+                        );?>
+                    </div>
+                <?endif;?>
 
             </div>
         </div>
     </section>
-<? $APPLICATION->IncludeFile('/local/include/blocks.abonements.php', ['ELEMENT_CODE' => 'trenazhernyy-zal-main', 'blockTitle' => "Подробнее о партнерах"], ['SHOW_BORDER' => false]); ?>
+<? $APPLICATION->IncludeFile('/local/include/blocks.abonements.php', ['ELEMENT_CODE' => 'partnery-club-members', 'blockTitle' => "Подробнее о партнерах"], ['SHOW_BORDER' => false]); ?>
 
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
