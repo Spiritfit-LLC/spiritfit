f06b4178101a3a8195fac8e46757d92e3a8bd8ac
Ilvir <i.harisov@spiritfit.ru>
2022-11-25 19:14:11 +0300

    Баннер и линия

--- local/components/custom/promocode.banner/templates/quiz.iphone/script.js
+++ local/components/custom/promocode.banner/templates/quiz.iphone/script.js
@@ -1,18 +1,20 @@
 $(document).ready(function(){
+    if (window.sessionStorage.getItem("bn_promocode")!=="1"){
+        setTimeout(function(){
+            $(".promocode-banner__container")
+                .fadeIn(300)
+                .css("display", "flex");
 
-    // if ()
-    $("a[href=\"#quiz\"]").click(function(){
-        $(".promocode-banner__container")
-            .fadeIn(300)
-            .css("display", "flex");
+            $('body').css('overflow', 'hidden');
+            dataLayerSend('bannerCatchDiscount', 'shown', bannerPromocodePage);
+            window.sessionStorage.setItem("bn_promocode", "1");
+        },bannerTime);
 
-        $('body').css('overflow', 'hidden');
-        dataLayerSend('bannerCatchDiscount', 'shown', bannerPromocodePage);
-    });
 
-    $('.promocode-banner__closer').on(clickHandler, function(){
-        $(".promocode-banner__container").fadeOut(300);
-        $('body').css('overflow', 'auto');
-        dataLayerSend('bannerCatchDiscount', 'clickCloseButton', bannerPromocodePage);
-    });
+        $('.promocode-banner__closer').on(clickHandler, function(){
+            $(".promocode-banner__container").fadeOut(300);
+            $('body').css('overflow', 'auto');
+            dataLayerSend('bannerCatchDiscount', 'clickCloseButton', bannerPromocodePage);
+        });
+    }
 });
