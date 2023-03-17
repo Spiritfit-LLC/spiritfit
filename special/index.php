37841acecab9d46465f8b3161d5961caf635c243
Ilvir <i.harisov@spiritfit.ru>
2023-03-17 12:14:23 +0300

    Абонементы special и общие

--- special/index.php
+++ special/index.php
@@ -22,7 +22,7 @@ $dbProperty = CIBlockProperty::GetList(array("SORT"=>"ASC"), array("ACTIVE"=>"Y"
 if ($property = $dbProperty->Fetch()){
     $dbEnum = CIBlockProperty::GetPropertyEnum($property["ID"], array(), array());
     while ($enum = $dbEnum->Fetch()){
-        if ($enum["XML_ID"]=="abonements"){
+        if ($enum["XML_ID"]=="special"){
             $property_show_in=$enum["ID"];
             $GLOBALS["arAbonementFilter"]["PROPERTY_SHOW_IN"]=$enum["ID"];
             break;
@@ -31,65 +31,65 @@ if ($property = $dbProperty->Fetch()){
 }
 
 ?>
-<div class="section-margin-top">
-<?$APPLICATION->IncludeComponent(
-    "bitrix:news.list",
-    "abonement.main",
-    Array(
-        "ACTIVE_DATE_FORMAT" => "d.m.Y",
-        "ADD_SECTIONS_CHAIN" => "N",
-        "AJAX_MODE" => "N",
-        "AJAX_OPTION_ADDITIONAL" => "",
-        "AJAX_OPTION_HISTORY" => "N",
-        "AJAX_OPTION_JUMP" => "N",
-        "AJAX_OPTION_STYLE" => "Y",
-        "CACHE_FILTER" => "N",
-        "CACHE_GROUPS" => "Y",
-        "CACHE_TIME" => "3600",
-        "CACHE_TYPE" => "A",
-        "CHECK_DATES" => "Y",
-        "DETAIL_URL" => "/abonement/#CODE#/",
-        "DISPLAY_BOTTOM_PAGER" => "N",
-        "DISPLAY_DATE" => "Y",
-        "DISPLAY_NAME" => "Y",
-        "DISPLAY_PICTURE" => "Y",
-        "DISPLAY_PREVIEW_TEXT" => "Y",
-        "DISPLAY_TOP_PAGER" => "N",
-        "FIELD_CODE" => array("CODE", "NAME", "PREVIEW_PICTURE", "IBLOCK_SECTION_CODE"),
-        "FILTER_NAME" => "arAbonementFilter",
-        "HIDE_LINK_WHEN_NO_DETAIL" => "N",
-        "IBLOCK_ID" => "9",
-        "IBLOCK_TYPE" => "content",
-        "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-        "INCLUDE_SUBSECTIONS" => "Y",
-        "MESSAGE_404" => "",
-        "NEWS_COUNT" => "",
-        "PAGER_BASE_LINK_ENABLE" => "N",
-        "PAGER_DESC_NUMBERING" => "N",
-        "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-        "PAGER_SHOW_ALL" => "N",
-        "PAGER_SHOW_ALWAYS" => "N",
-        "PAGER_TEMPLATE" => ".default",
-        "PAGER_TITLE" => "Новости",
-        "PARENT_SECTION" => "",
-        "PARENT_SECTION_CODE" => "",
-        "PREVIEW_TRUNCATE_LEN" => "",
-        "PROPERTY_CODE" => array("BASE_PRICE", "INCLUDE", "ADDITIONAL_CLASS", "PRICE_SIGN", "HIDDEN", "FOR_PRESENT", "PRICE", "TITLE"),
-        "SET_BROWSER_TITLE" => "N",
-        "SET_LAST_MODIFIED" => "N",
-        "SET_META_DESCRIPTION" => "N",
-        "SET_META_KEYWORDS" => "N",
-        "SET_STATUS_404" => "N",
-        "SET_TITLE" => "N",
-        "SHOW_404" => "N",
-        "SORT_BY1" => "SORT",
-        "SORT_BY2" => "",
-        "SORT_ORDER1" => "ASC",
-        "SORT_ORDER2" => "",
-        "STRICT_SECTION_CHECK" => "N"
-    )
-);?>
-</div>
+    <div class="section-margin-top">
+        <?$APPLICATION->IncludeComponent(
+            "bitrix:news.list",
+            "abonement.main",
+            Array(
+                "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                "ADD_SECTIONS_CHAIN" => "N",
+                "AJAX_MODE" => "N",
+                "AJAX_OPTION_ADDITIONAL" => "",
+                "AJAX_OPTION_HISTORY" => "N",
+                "AJAX_OPTION_JUMP" => "N",
+                "AJAX_OPTION_STYLE" => "Y",
+                "CACHE_FILTER" => "N",
+                "CACHE_GROUPS" => "Y",
+                "CACHE_TIME" => "3600",
+                "CACHE_TYPE" => "A",
+                "CHECK_DATES" => "Y",
+                "DETAIL_URL" => "/abonement/#CODE#/",
+                "DISPLAY_BOTTOM_PAGER" => "N",
+                "DISPLAY_DATE" => "Y",
+                "DISPLAY_NAME" => "Y",
+                "DISPLAY_PICTURE" => "Y",
+                "DISPLAY_PREVIEW_TEXT" => "Y",
+                "DISPLAY_TOP_PAGER" => "N",
+                "FIELD_CODE" => array("CODE", "NAME", "PREVIEW_PICTURE", "IBLOCK_SECTION_CODE"),
+                "FILTER_NAME" => "arAbonementFilter",
+                "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                "IBLOCK_ID" => "9",
+                "IBLOCK_TYPE" => "content",
+                "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                "INCLUDE_SUBSECTIONS" => "Y",
+                "MESSAGE_404" => "",
+                "NEWS_COUNT" => "",
+                "PAGER_BASE_LINK_ENABLE" => "N",
+                "PAGER_DESC_NUMBERING" => "N",
+                "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                "PAGER_SHOW_ALL" => "N",
+                "PAGER_SHOW_ALWAYS" => "N",
+                "PAGER_TEMPLATE" => ".default",
+                "PAGER_TITLE" => "Новости",
+                "PARENT_SECTION" => "",
+                "PARENT_SECTION_CODE" => "",
+                "PREVIEW_TRUNCATE_LEN" => "",
+                "PROPERTY_CODE" => array("BASE_PRICE", "INCLUDE", "ADDITIONAL_CLASS", "PRICE_SIGN", "HIDDEN", "FOR_PRESENT", "PRICE", "TITLE"),
+                "SET_BROWSER_TITLE" => "N",
+                "SET_LAST_MODIFIED" => "N",
+                "SET_META_DESCRIPTION" => "N",
+                "SET_META_KEYWORDS" => "N",
+                "SET_STATUS_404" => "N",
+                "SET_TITLE" => "N",
+                "SHOW_404" => "N",
+                "SORT_BY1" => "SORT",
+                "SORT_BY2" => "",
+                "SORT_ORDER1" => "ASC",
+                "SORT_ORDER2" => "",
+                "STRICT_SECTION_CHECK" => "N"
+            )
+        );?>
+    </div>
 
 
 <? $APPLICATION->IncludeComponent(
