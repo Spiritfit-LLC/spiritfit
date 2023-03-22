4bb85df2abb0049b88c29f98bed31b57870b1085
Ilvir <i.harisov@spiritfit.ru>
2023-03-22 13:15:02 +0300

    Обновил шаблон метатегов на странице акций

--- local/templates/spiritfit-v3/components/bitrix/news.detail/stock/result_modifier.php
+++ local/templates/spiritfit-v3/components/bitrix/news.detail/stock/result_modifier.php
@@ -1,4 +1,14 @@
 <?php
 global $APPLICATION;
 $APPLICATION->AddViewContent('inhead', $arResult['DETAIL_PICTURE']['SRC']);
+
+$APPLICATION->SetTitle($arResult["NAME"]);
+if (empty($arResult["META_ELEMENT_TITLE"])){
+    $title = $arResult["NAME"]." - Акции в Spirit Fitness";
+    $APPLICATION->SetPageProperty("title", $title);
+}
+if (empty($arResult["META_ELEMENT_DESCRIPTION"])){
+    $description = $arResult["NAME"].". Акции и скидки на абонемент в фитнес-клуб в Москве &#128165; Бесплатная пробная тренировка &#128293; Запишитесь прямо сейчас!";
+    $APPLICATION->SetPageProperty("description", $description);
+}
 ?>
